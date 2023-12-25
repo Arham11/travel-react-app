4362a66722fc856ee94a1e2c28f0c1c7ecdc2d40
Arham Chowdhry <arham.chowdhry@abbott.com>
2023-12-25 12:36:33 +0530

    added delete, sort , toggle funtionalities

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "charger", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 12, packed: false },
 ];
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    console.log(id);
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    if (window.confirm("Are you sure you want to delete all the items")) {
+      setItems([]);
+    }
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <Packlist items={items} />
-      <Stats />
+      <Packlist
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -69,32 +92,76 @@ function Form({ onAddItems }) {
   );
 }
 
-function Packlist({ items }) {
+function Packlist({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems = [];
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  /*if (sortBy === "packed")
+    items.forEach((item) => {
+      item.packed
+        ? sortedItems.push(item)
+        :  sortedItems = [item, ...sortedItems];
+    });*/
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.length &&
-          items.map((item) => <Item key={item.id} item={item} />)}
+        {sortedItems.length &&
+          sortedItems.map((item) => (
+            <Item
+              key={item.id}
+              item={item}
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+            />
+          ))}
       </ul>
+      {/* (e) => changeSorting(e) */}
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By descripton</option>
+          <option value="packed">Sort By Packing</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear List</button>
+      </div>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  let totalItems = items.length;
+  let packedItem = items && items.filter((item) => item.packed).length;
+  let percent = (packedItem / totalItems) * 100;
   return (
     <footer className="stats">
-      💼 You have X item in your list, and you have already packed X (x%)
+      💼 You have {totalItems} item in your list, and you have already packed
+      {packedItem} {percent} %
     </footer>
   );
 }
